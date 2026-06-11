4b7047948ace45ae1e7d15e987dce625d2229063
Tomáš Dunaj <dunaj7@stud.uniza.sk>
2026-06-11 18:04:06 +0200

    feat: create CvikRepository and TreningovyZaznamRepository

---
+++ src/main/java/sk/tomas/fitness_tracker/model/CvikRepository.java
@@ -0,0 +1,6 @@
+package sk.tomas.fitness_tracker.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CvikRepository extends JpaRepository<Cvik, Long> {
+}
---
+++ src/main/java/sk/tomas/fitness_tracker/model/TreningovyZaznamRepository.java
@@ -0,0 +1,6 @@
+package sk.tomas.fitness_tracker.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TreningovyZaznamRepository extends JpaRepository<TreningovyZaznam, Long> {
+}
