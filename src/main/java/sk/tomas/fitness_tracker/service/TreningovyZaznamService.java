e77d8a3285b1922df022ea14c719a9bdccf886c0
Tomáš Dunaj <dunaj7@stud.uniza.sk>
2026-06-11 18:12:16 +0200

    feat: create TreningovyZaznamService with fetch and save logic

---
+++ src/main/java/sk/tomas/fitness_tracker/service/TreningovyZaznamService.java
@@ -0,0 +1,23 @@
+package sk.tomas.fitness_tracker.service;
+
+import sk.tomas.fitness_tracker.model.TreningovyZaznam;
+import sk.tomas.fitness_tracker.model.TreningovyZaznamRepository;
+
+import java.util.List;
+
+public class TreningovyZaznamService {
+
+    private final TreningovyZaznamRepository treningovyZaznamRepository;
+
+    public TreningovyZaznamService(TreningovyZaznamRepository treningovyZaznamRepository) {
+        this.treningovyZaznamRepository = treningovyZaznamRepository;
+    }
+
+    public List<TreningovyZaznam> getVsetkyZaznamy() {
+        return treningovyZaznamRepository.findAll();
+    }
+
+    public TreningovyZaznam ulozZaznam(TreningovyZaznam novyZaznam) {
+        return this.treningovyZaznamRepository.save(novyZaznam);
+    }
+}
